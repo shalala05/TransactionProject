0241f697198651361d1e6d5be01c28501ff9315c
shalala05 <allahverdiyevashelale05@gmail.com>
2024-11-16 05:01:58 +0400

    fourth

--- TransactionProject/app.js
+++ TransactionProject/app.js
@@ -1,20 +1,68 @@
+const modal = document.querySelector(".modal");
 const transactionList = document.querySelector(".transaction-list");
-fetch('https://acb-api.algoritmika.org/api/transaction')
-  .then((response) => response.json())
-  .then((data) => {
-    if (Array.isArray(data)) {
-      data.forEach((transaction) => {
-        const li = document.createElement("li");
-        const template = `
-          <span>From: ${transaction.from}</span>
-          <span>To: ${transaction.to}</span>
-          <span>Amount: ${transaction.amount}</span>
-        `;
-        li.innerHTML = template;
-        transactionList.appendChild(li);
-      });
-    } else {
-      console.error("Data massiv deyil");
-    }
+const [fromInput, toInput, amountInput] = document.querySelectorAll(".from, .to, .amount");
+
+const toggleModal = () => modal.classList.toggle("hidden");
+
+const createTransactionElement = (transaction) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+        <span>From: ${transaction.from}</span>
+        <span>To: ${transaction.to}</span>
+        <span>Amount: ${transaction.amount}</span>
+        <div class="transaction-actions">
+            <button class="edit-btn">Edit</button>
+            <button class="delete-btn">Delete</button>
+        </div>
+    `;
+
+  li.querySelector(".delete-btn").onclick = () => {
+    fetch(`https://acb-api.algoritmika.org/api/transaction/${transaction.id}`, { method: "DELETE" })
+      .then(() => li.remove())
+      .catch(console.error);
+  };
+
+  li.querySelector(".edit-btn").onclick = () => {
+    [fromInput.value, toInput.value, amountInput.value] = [transaction.from, transaction.to, transaction.amount];
+    toggleModal();
+    document.querySelector(".save-btn").onclick = () => {
+      fetch(`https://acb-api.algoritmika.org/api/transaction/${transaction.id}`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ from: fromInput.value, to: toInput.value, amount: amountInput.value }),
+      })
+        .then(() => {
+          li.querySelector("span:nth-child(1)").textContent = `From: ${fromInput.value}`;
+          li.querySelector("span:nth-child(2)").textContent = `To: ${toInput.value}`;
+          li.querySelector("span:nth-child(3)").textContent = `Amount: ${amountInput.value}`;
+          toggleModal();
+        })
+        .catch(console.error);
+    };
+  };
+
+  return li;
+};
+
+fetch("https://acb-api.algoritmika.org/api/transaction")
+  .then((res) => res.json())
+  .then((data) => data.forEach((t) => transactionList.appendChild(createTransactionElement(t))))
+  .catch(console.error);
+
+document.querySelector(".add-btn").onclick = toggleModal;
+document.querySelector(".close-btn").onclick = toggleModal;
+
+document.querySelector(".save-btn").onclick = () => {
+  const newTransaction = { from: fromInput.value, to: toInput.value, amount: amountInput.value };
+  fetch("https://acb-api.algoritmika.org/api/transaction", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newTransaction),
   })
-  .catch((error) => console.error(error));
+    .then((res) => res.json())
+    .then((data) => {
+      transactionList.appendChild(createTransactionElement(data));
+      toggleModal();
+    })
+    .catch(console.error);
+};
